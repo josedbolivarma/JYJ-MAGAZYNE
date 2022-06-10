33484a9aa3be4c1b472edba35322b2490dde157c
Hipns1 <jesudpf21@hotmail.com>
2022-06-10 10:09:56 -0500

    Formato de flujo en redux para productos

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Card = () => {
+
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <div>Card</div>
   )
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const google = new GoogleAuthProvider();
-const facebook = new FacebookAuthProvider();
 const db = getFirestore();
 
 export {
