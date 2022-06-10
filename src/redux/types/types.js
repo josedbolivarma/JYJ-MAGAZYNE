ee61e17fdcdae2966bc790c76075fa149c44bd01
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-06-10 13:23:45 -0500

    login & register reducer

--- src/redux/types/types.js
+++ src/redux/types/types.js
@@ -1,19 +1,20 @@
+//Types del LOGIN
 export const typesLogin = {
-    login: 'login',
-    logout: 'logout',
-
+  login: 'login',
+  logout: 'logout',
 }
 
+//Types del REGISTER
 export const typesRegister = {
-    register: 'register'
+  register: 'register',
 }
 
 export const typesProductos = {
-    list: 'list'
+  list: 'list',
 }
 
 export const typesShoppingCart = {
-    add: 'add [SHOPPING CART]',
-    remove: 'remove [SHOPPING CART]',
-    list: 'get [SHOPPING CART]'
+  add: 'add [SHOPPING CART]',
+  remove: 'remove [SHOPPING CART]',
+  list: 'get [SHOPPING CART]',
 }
