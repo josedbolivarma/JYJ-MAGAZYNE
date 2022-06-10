1d2152e53c86147955250d3b5a9591e3fe402d08
Hipns1 <jesudpf21@hotmail.com>
2022-06-10 10:40:26 -0500

    Getproducts

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,28 @@
 import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { listAsync } from '../redux/actions/actionProducts'
 
 const Card = () => {
 
+  const dispatch = useDispatch();
+  const { products } = useSelector(state => state.products);
+
   useEffect(() => {
-    
+    dispatch(listAsync());
   }, [])
 
+  console.log(products);
+
   return (
-    <div>Card</div>
+    <div>
+      {
+        products.map(product => (
+          <div key={product.nombre}>
+            <h1>{product.nombre}</h1>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
--- src/routes/DashboardRoutes.js
+++ src/routes/DashboardRoutes.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import { Routes } from 'react-router-dom'
+import Card from '../components/Card'
 import Header from '../components/Header'
-import Home from '../pages/Home'
 
 const DashboardRoutes = () => {
   return (
     <>
-    <Header />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="*" element={<Navigate to='/' />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Card />} />
+        <Route path="*" element={<Navigate to='/' />} />
+      </Routes>
     </>
   )
 }
