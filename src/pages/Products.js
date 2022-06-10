9f6ebde3c743b6c257a6a71ef3eecb122928c5f1
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-06-10 11:41:35 -0500

    change styles product pages

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,9 @@
-import { FilterProducts } from '../components/FilterProducts'
 import { ListProducts } from '../components/ListProducts'
-
+import styles from '../styled/ListProducts.module.scss'
 export function Products() {
   return (
-    <section>
-      <FilterProducts />
-      <div className=''>lista de productos</div>
+    <section className={styles.layoutProducts}>
+      <h1 className={styles.titlePage}>Catalogo De Productos</h1>
       <ListProducts />
     </section>
   )
