d52787b134850679c461354e0c72bfc952df63fe
Jose David <josedbolivarmafrontend@gmail.com>
2022-06-10 09:57:03 -0500

    Store Products

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,23 +1,25 @@
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBi1H4s3NKy8PCQILrD1O00u7r5658IzDI",
-  authDomain: "catapi-4e055.firebaseapp.com",
-  projectId: "catapi-4e055",
-  storageBucket: "catapi-4e055.appspot.com",
-  messagingSenderId: "425276761208",
-  appId: "1:425276761208:web:6f75ad38c3b7c26c00398d"
+    apiKey: "AIzaSyBHK3ir42Gz236xIIYvdqk96uVRWNVhuZQ",
+    authDomain: "buonafit-8d2b5.firebaseapp.com",
+    projectId: "buonafit-8d2b5",
+    storageBucket: "buonafit-8d2b5.appspot.com",
+    messagingSenderId: "935986322247",
+    appId: "1:935986322247:web:f5b0b4c93a33dd73c4ca62"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const google = new GoogleAuthProvider();
+const facebook = new FacebookAuthProvider();
 const db = getFirestore();
 
 export {
   app, 
   google, 
+  facebook,
   db
 }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,12 +2,17 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { loginReducer } from "../reducers/loginReducer";
 import { registerReducer } from "../reducers/registerReducer";
+import { shoppingCartReducer } from "../reducers/shoppingCartReducer";
+import { productsReducer } from "../reducers/productsReducer";
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducersEnviar = combineReducers({
     login: loginReducer,
     register: registerReducer,
+    products: productsReducer,
+    cart: shoppingCartReducer,
+
 })
 
 export const store = createStore(
